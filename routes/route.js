659b19cc27aabb3ebfd80d20f79022228ba9f8e4
Luca <lucatonello097@gmail.com>
2024-09-14 15:33:56 -0300

    CRUD routes for comments as well

--- routes/route.js
+++ routes/route.js
@@ -7,27 +7,95 @@ router.get('/', (req, res) => {
 });
 
 router.get('/posts', async (req, res) => {
-    const result = await pool.query('SELECT * FROM posts');
-    res.json(result.rows);
+    try {
+        const result = await pool.query('SELECT * FROM posts');
+        res.json(result.rows);
+    } catch (err) {
+        console.error(err);
+        res.status(500).send('Error loading posts')
+    }
 });
 
 router.post('/posts', async (req, res) => {
-    const text = req.body.text;
-    const authorid = req.body.authorid;
-    await pool.query('INSERT INTO posts (text, authorid) VALUES ($1, $2)', [text, authorid]);
-
-    res.json({ message: 'post created' })
+    try {
+        const text = req.body.text;
+        const authorid = req.body.authorid;
+        await pool.query('INSERT INTO posts (text, authorid) VALUES ($1, $2)', [text, authorid]);
+    
+        res.json({ message: 'post created' })
+    
+    } catch (err) {
+        console.error(err);
+        res.status(500).send('Error posting the post');
+    }
 });
 
 router.put('/posts/:id', async (req, res) => {
-    const newText = req.body.newText;
-    const postid = req.params.id;
-    await pool.query('UPDATE posts SET text = $1 WHERE id = $2', [newText, postid]);
+    try {
+        const text = req.body.text;
+        const postid = req.params.id;
+        await pool.query('UPDATE posts SET text = $1 WHERE id = $2', [newText, postid]);
+    
+        res.json({ message: 'post updated' });    
+    } catch (err) {
+        console.error(err);
+        res.status(500).send('Error updating the post');
+    }
 });
 
-router.delete('/posts/:id', async(req, res) => {
-    const postid = req.params.id;
-    await pool.query('DELETE FROM posts WHERE id = $1', [postid]);
+router.delete('/posts/:id', async (req, res) => {
+    try {
+        const postid = req.params.id;
+        await pool.query('DELETE FROM posts WHERE id = $1', [postid]);
+    
+        res.json({ message: 'post deleted'});
+    } catch (err) {
+        console.error(err);
+        res.status(500).send('Error deleting the post');
+    }
+});
+
+///R - GET posts/:id/comments
+///C - POST posts/:id/comments
+///U - PUT posts/:id/comments/:commentid
+///D - DELETE posts/:id/comments/:commentid
+
+router.get('/posts/:id/comments', async (req, res) => {
+    try {
+        const result = await pool.query('SELECT * FROM comments');
+        res.json(result.rows);
+    } catch (err) {
+        console.error(err);
+        res.status(500).send('Error loading comments');
+    }
+
+});
+
+router.post('posts/:id/comments', async (req, res) => {
+    try {
+        const text = req.body.text;
+        const postid = req.params.id;
+        const authorid = req.body.authorid;
+
+        await pool.query('INSERT INTO comments (text, postid, authorid) VALUES ($1, $2, $3)', [text, postid, authorid]);
+        res.json({ message: 'comment posted'})
+    } catch (err) {
+        console.error(err);
+        res.status(500).send('Error posting the comment');
+    }
+});
+
+router.put('/posts/:id/comments/:commentid', async (req, res) => {
+    try {
+        const text = req.body.text;
+        const commentid = req.params.commentid;
+
+        await pool.query('UPDATE comments SET text = $1 WHERE commentid = $2', [text, commentid]);
+        res.json({ message: 'comment updated'});
+    } catch (err) {
+        console.error(err);
+        res.status(500).send('Error posting the comment');
+    }
 });
 
 module.exports = router;
